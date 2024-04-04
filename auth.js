dbe6d99d85519b088cf4648f9830c27d8ea95d32
tanishka gupta <95035224+tanihka@users.noreply.github.com>
2024-04-04 10:14:22 +0530

    Update auth.js
    modification

--- auth.js
+++ auth.js
@@ -2,8 +2,8 @@ const { OAuth2Client } = require('google-auth-library');
 
 // OAuth2 client configuration
 const oauth2Client = new OAuth2Client({
-  clientId: '15679233869-etaov50t6439586756jsd9p4e0bicvq7.apps.googleusercontent.com',
-  clientSecret: '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',
+  clientId: 'ADD_YOUR_ID',
+  clientSecret: 'ADD_YOUR_SECRET',
   redirectUri: 'http://localhost:3000/callback', 
 });
 
@@ -22,8 +22,8 @@ async function refreshAccessToken() {
 
 // Setting access and refresh tokens
 oauth2Client.setCredentials({
-  access_token: 'ya29.a0AfB_byDEmsxYl29fBElEaUrcHhBIQ7mQHavMddwFJ07ocuIxnp8wMiOaOVnWzi16A0YiINznmMclwewr50nsae4X27qLYMCsgWU1pBODN_muCgyVBeasSbT5Fkb1TCYj_fNBFuxufNqrCenUMpgWjpVHERbATKtrCqfe3AaCgYKAWoSARESFQGOcNnCX_VIsndwcRuuXpdqBWKC-w0173',
-  refresh_token: '1//04an1a2UFujdyCgYIARAAGAQSNwF-L9IrCiM92OLF5Yztpl2N61VC9P-MuOiFnKUui6AecTdglr0rHAZwRRKfE252yQqi83PYyL0',
+  access_token: 'YOURS_ACCESS_TOKEN',
+  refresh_token: 'YOURS_REFRESH_TOKEN',
 });
 
 
